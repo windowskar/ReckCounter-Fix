ed2b08ff73824b619b9672d4d5050ee9b848f114
windowskar <oskar.beneke@iver.se>
2026-05-07 14:57:01 +0200

    Add files via upload

---
+++ ReckCounter/Localization.lua
@@ -0,0 +1,18 @@
+-- Localization file for ReckCounter
+if ( GetLocale() == "deDE" ) then
+    RECKCOUNTER_RECKONING = "Abrechnung";
+    RECKCOUNTER_YOUHIT = "You hit";
+    RECKCOUNTER_YOUCRIT = "You crit";
+elseif ( GetLocale() == "frFR") then
+    RECKCOUNTER_RECKONING = "Reckoning";
+    RECKCOUNTER_YOUHIT = "You hit";
+    RECKCOUNTER_YOUCRIT = "You crit";
+elseif ( GetLocale() == "krKR" ) then
+    RECKCOUNTER_RECKONING = "Reckoning";
+    RECKCOUNTER_YOUHIT = "You hit";
+    RECKCOUNTER_YOUCRIT = "You crit";
+else
+    RECKCOUNTER_RECKONING = "Reckoning";
+    RECKCOUNTER_YOUHIT = "You hit";
+    RECKCOUNTER_YOUCRIT = "You crit";
+end
